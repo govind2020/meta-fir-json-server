21b18d6cc4e2645c75a6d59f736d8917833f897a
arvindalawa07 <109585417+arvindalawa07@users.noreply.github.com>
2024-03-14 14:39:50 +0530

    add index file

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+require('dotenv').config();
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200 ;// you can use any port number here; i chose to use 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port,()=>console.log("Server running on ",port));
